07edee5faf5dd0d28260ade511758a0d03ac5c32
Bruno Diaz <brunjo12@hotmail.com>
2024-03-19 15:59:49 -0500

    fix: format

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,15 +6,15 @@ interface Props extends TodoType {
 
 export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
   return (
-        <div className="view">
-            <input
-                className="toggle"
-                checked={completed}
-                type="checkbox"
-                onChange={() => {}}
-            />
-            <label>{title}</label>
-            <button className='destroy' onClick={() => { onRemoveTodo(id) }} />
-        </div>
+    <div className="view">
+        <input
+            className="toggle"
+            checked={completed}
+            type="checkbox"
+            onChange={() => {}}
+        />
+        <label>{title}</label>
+        <button className='destroy' onClick={() => { onRemoveTodo(id) }} />
+    </div>
   )
 }
