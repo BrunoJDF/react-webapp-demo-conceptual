24a770644ef7de94d0acc030de9b7c5a43e0a4ab
Bruno Diaz <brunjo12@hotmail.com>
2024-03-21 10:46:22 -0500

    feat: add clear completed

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,11 @@ const App = (): JSX.Element => {
   const handleFilterChange = (filter: FilterValue): void => {
     setFilterSelected(filter)
   }
+  const handleRemoveAllCompleted = (): void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
   const activeCount = todos.filter(todo => !todo.completed).length
   const completedCount = todos.length - activeCount
 
@@ -68,7 +73,7 @@ const App = (): JSX.Element => {
         activeCount={activeCount}
         completedCount={completedCount}
         filterSelected={filterSelected}
-        onClearCompleted={() => {}}
+        onClearCompleted={handleRemoveAllCompleted}
         handleFilterChange={handleFilterChange}
       />
     </div>
